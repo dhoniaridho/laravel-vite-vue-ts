313b320c2d21aef07636fea61e8d4f2ab2f9d51b
dhoniaridho <dhoniaridho@gmail.com>
2022-08-09 22:59:59 +0700

    feat: add validation on login and register

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import BreezeButton from "@/Components/Button.vue";
-import BreezeCheckbox from "@/Components/Checkbox.vue";
 import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
 import route from "ziggy-js";
 
@@ -25,58 +23,83 @@ const submit = () => {
 <template>
     <Head title="Log in" />
 
-    <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-        {{ status }}
+    <div class="flex justify-center items-center min-h-screen">
+        <n-card
+            title="Login"
+            class="shadow-md"
+            style="max-width: 600px; margin: auto"
+        >
+            <form @submit.prevent="submit">
+                <n-form-item
+                    ref="email"
+                    first
+                    id="email"
+                    path="reenteredPassword"
+                    label="Email"
+                >
+                    <n-input
+                        id="email"
+                        type="email"
+                        v-model:value="form.email"
+                        required
+                        autofocus
+                        autocomplete="username"
+                        placeholder="Input Email"
+                        :loading="form.processing"
+                        :status="form.errors.email ? 'error' : 'success'"
+                    />
+                    <template #feedback>
+                        <n-text type="error">{{ form.errors.email }}</n-text>
+                    </template>
+                </n-form-item>
+                <n-form-item
+                    ref="password"
+                    first
+                    path="reenteredPassword"
+                    label="Password"
+                >
+                    <n-input
+                        id="password"
+                        type="password"
+                        v-model:value="form.password"
+                        required
+                        autocomplete="current-password"
+                        :loading="form.processing"
+                        :status="form.errors.password ? 'error' : 'success'"
+                    />
+                    <template #feedback>
+                        <n-text type="error">{{ form.errors.password }}</n-text>
+                    </template>
+                </n-form-item>
+                <n-checkbox class="mt-6" v-model:checked="form.remember">
+                    Remember me
+                </n-checkbox>
+
+                <div class="flex items-center justify-end mt-4">
+                    <Link
+                        v-if="canResetPassword"
+                        :href="route('password.request')"
+                        class="underline text-sm text-gray-600 hover:text-gray-900"
+                    >
+                        Forgot your password?
+                    </Link>
+
+                    <n-button
+                        attr-type="submit"
+                        class="ml-4"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing"
+                    >
+                        Log in
+                    </n-button>
+                </div>
+            </form>
+        </n-card>
     </div>
-
-    <form @submit.prevent="submit">
-        <div>
-            <n-input
-                id="email"
-                type="email"
-                class="mt-1 block w-full"
-                v-model:value="form.email"
-                required
-                autofocus
-                autocomplete="username"
-            />
-        </div>
-
-        <div class="mt-4">
-            <n-input
-                id="password"
-                type="password"
-                class="mt-1 block w-full"
-                v-model:value="form.password"
-                required
-                autocomplete="current-password"
-            />
-        </div>
-
-        <div class="block mt-4">
-            <label class="flex items-center">
-                <BreezeCheckbox name="remember" v-model="form.remember" />
-                <span class="ml-2 text-sm text-gray-600">Remember me</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <Link
-                v-if="canResetPassword"
-                :href="route('password.request')"
-                class="underline text-sm text-gray-600 hover:text-gray-900"
-            >
-                Forgot your password?
-            </Link>
-
-            <n-button
-                attr-type="submit"
-                class="ml-4"
-                :class="{ 'opacity-25': form.processing }"
-                :disabled="form.processing"
-            >
-                Log in
-            </n-button>
-        </div>
-    </form>
 </template>
+
+<style module>
+.login-wrapper {
+    margin: auto;
+}
+</style>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,62 +1,134 @@
 <script setup>
-import BreezeButton from '@/Components/Button.vue';
-import BreezeGuestLayout from '@/Layouts/Guest.vue';
-import BreezeInput from '@/Components/Input.vue';
-import BreezeLabel from '@/Components/Label.vue';
-import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
-import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
 
 const form = useForm({
-    name: '',
-    email: '',
-    password: '',
-    password_confirmation: '',
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
     terms: false,
 });
 
 const submit = () => {
-    form.post(route('register'), {
-        onFinish: () => form.reset('password', 'password_confirmation'),
+    form.post(route("register"), {
+        onFinish: () => form.reset("password", "password_confirmation"),
     });
 };
 </script>
 
 <template>
-    <BreezeGuestLayout>
-        <Head title="Register" />
+    <div class="flex justify-center items-center min-h-screen">
+        <n-card
+            title="Register"
+            class="shadow-md"
+            style="max-width: 600px; margin: auto"
+        >
+            <form @submit.prevent="submit">
+                <n-form-item
+                    ref="name"
+                    first
+                    id="name"
+                    path="reenteredPassword"
+                    label="Name"
+                >
+                    <n-input
+                        id="name"
+                        type="text"
+                        v-model:value="form.name"
+                        required
+                        autofocus
+                        autocomplete="username"
+                        placeholder="Input Name"
+                        :loading="form.processing"
+                        :status="form.errors.name ? 'error' : 'success'"
+                    />
+                    <template #feedback>
+                        <n-text type="error">{{ form.errors.name }}</n-text>
+                    </template>
+                </n-form-item>
+                <n-form-item
+                    ref="email"
+                    first
+                    id="email"
+                    path="reenteredPassword"
+                    label="Email"
+                >
+                    <n-input
+                        id="email"
+                        type="text"
+                        v-model:value="form.email"
+                        required
+                        autofocus
+                        autocomplete="username"
+                        placeholder="Input Email"
+                        :loading="form.processing"
+                        :status="form.errors.email ? 'error' : 'success'"
+                    />
+                    <template #feedback>
+                        <n-text type="error">{{ form.errors.email }}</n-text>
+                    </template>
+                </n-form-item>
+                <n-form-item
+                    ref="password"
+                    first
+                    path="reenteredPassword"
+                    label="Password"
+                >
+                    <n-input
+                        id="password"
+                        type="password"
+                        style="min-width: 100%"
+                        v-model:value="form.password"
+                        required
+                        placeholder="Input Password"
+                        autocomplete="current-password"
+                        :loading="form.processing"
+                        :status="form.errors.password ? 'error' : 'success'"
+                    />
+                    <template #feedback>
+                        <n-text type="error">{{ form.errors.password }}</n-text>
+                    </template>
+                </n-form-item>
+                <n-form-item
+                    ref="password_confirmation"
+                    first
+                    path="password_confirmation"
+                    label="Password Confirmation"
+                >
+                    <n-input
+                        id="password_confirmation"
+                        type="password"
+                        placeholder="Input Password Confirmation"
+                        v-model:value="form.password_confirmation"
+                        required
+                        autocomplete="password_confirmation"
+                        :loading="form.processing"
+                        :status="form.errors.password_confirmation ? 'error' : 'success'"
+                    />
+                    <template #feedback>
+                        <n-text type="error">{{ form.errors.password_confirmation }}</n-text>
+                    </template>
+                </n-form-item>
 
-        <BreezeValidationErrors class="mb-4" />
+                <div class="flex items-center justify-end mt-4">
+                    <Link
+                        v-if="canResetPassword"
+                        :href="route('password.request')"
+                        class="underline text-sm text-gray-600 hover:text-gray-900"
+                    >
+                        Forgot your password?
+                    </Link>
 
-        <form @submit.prevent="submit">
-            <div>
-                <BreezeLabel for="name" value="Name" />
-                <BreezeInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
-            </div>
-
-            <div class="mt-4">
-                <BreezeLabel for="email" value="Email" />
-                <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autocomplete="username" />
-            </div>
-
-            <div class="mt-4">
-                <BreezeLabel for="password" value="Password" />
-                <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <BreezeLabel for="password_confirmation" value="Confirm Password" />
-                <BreezeInput id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Already registered?
-                </Link>
-
-                <BreezeButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </BreezeButton>
-            </div>
-        </form>
-    </BreezeGuestLayout>
+                    <n-button
+                        attr-type="submit"
+                        class="ml-4"
+                        :class="{ 'opacity-25': form.processing }"
+                        :disabled="form.processing"
+                    >
+                        Register
+                    </n-button>
+                </div>
+            </form>
+        </n-card>
+    </div>
 </template>
